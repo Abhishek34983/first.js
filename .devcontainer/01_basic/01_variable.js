d1dabe814aacb457920e39f800c45f358fba6c35
Abhishek34983 <71623690+Abhishek34983@users.noreply.github.com>
2023-11-04 14:27:25 +0000

    Variable parts

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,9 @@
+
+const account_Id = 123
+let account_Email = "abhishek@google.com"
+var account_Password = "0000"
+account_City = "Mumbai"
+
+console.log(account_Id);
+
+console.table([account_Email,account_Id,account_Password,account_City])
