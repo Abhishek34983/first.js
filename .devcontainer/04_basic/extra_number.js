0b5be8d5c66198add7f28ac4d8a9c56a6b96a5f6
Abhishek34983 <71623690+Abhishek34983@users.noreply.github.com>
2024-01-17 07:29:48 +0000

    new

--- .devcontainer/04_basic/extra_number.js
+++ .devcontainer/04_basic/extra_number.js
@@ -1,15 +1,13 @@
-function findLocalPeak(N, arr) {
-    var result=[];
-    if (N == 1) {result.push(arr[0]);};
-    if (arr[0]>arr[1]) {result.push(arr[0]);}
-    if (arr[N-1] >= arr[N-2]) {result.push(arr[N-1]);}
-    for (let i=1;i<=arr.length;i++){
-        if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
-            result.push(arr[i]);
+function extractNumbers(N, arr) {
+let result = [];
+   for(let i=0;i<N;i++){
+       if (typeof arr[i] ==='number')
+       {
+           result.push(arr[i]);
         }
     }
     return result;
 }
 
-let arr = [791, 268, 468, 886, 388, 2, 452, 102, 445, 478, 551, 239, 861, 611, 716, 995, 897, 252, 170, 217, 409, 112, 939];
-console.log(findLocalPeak(23,arr));
+let arr = ['a',3,'long',17,'crio','dsa',100,20];
+console.log(extractNumbers(8,arr));
