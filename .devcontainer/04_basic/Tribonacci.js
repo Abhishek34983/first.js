f8e32b0e6022975bfed794f2ba158393418fd55c
Abhishek34983 <71623690+Abhishek34983@users.noreply.github.com>
2024-01-28 14:47:57 +0000

    new

--- .devcontainer/04_basic/Tribonacci.js
+++ .devcontainer/04_basic/Tribonacci.js
@@ -5,3 +5,18 @@ function ne(n){
 }
 
 console.log(ne(2));
+
+function countDown(n) {
+  if (n <= 0) return;
+  console.log(n);
+  countDown(n - 1);
+}
+
+countDown(5);
+
+function generatePascalTriangle(row, col) {
+  if (col === 0 || col === row) return 1;
+  return generatePascalTriangle(row - 1, col - 1) + generatePascalTriangle(row - 1, col);
+}
+
+const result = generatePascalTriangle(5, 2);
