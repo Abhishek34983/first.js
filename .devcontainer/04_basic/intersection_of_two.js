8d2641a585a95b51bc5bdd4ec1027fc3c9818a3e
Abhishek34983 <71623690+Abhishek34983@users.noreply.github.com>
2024-02-05 11:04:15 +0000

    updated

--- .devcontainer/04_basic/intersection_of_two.js
+++ .devcontainer/04_basic/intersection_of_two.js
@@ -26,18 +26,21 @@ function intersectionOfTwoArrays(n,nums1,m, nums2){
     }
     console.log(map2);
 
-    let arr = [];
-    for (let [key, value] of map1){
-        let arr = [];
-        if(map2.has(key)){
-            console.log(key);
-            if()
+    //let arr = [];
+    let intersection = [];
+    for (let [key, value] of map1) {
+        if (map2.has(key)) {
+            const countInBothArrays = Math.min(value, map2.get(key));
+            for (let i = 0; i < countInBothArrays; i++) {
+                intersection.push(key);
+            }
         }
-            };
-    return arr;
+    }
+    intersection.sort(function(a, b){return a - b});
+    return intersection;
 };
 
-let nums1 = [1,2,2,1];
-let nums2 = [2,2,3];
+let nums1 = [-85, 32, -89, 1, 16, -47, -3, -63, 16, -39, -50, 2, -63, 14, 13, -44, -43, 40, -5, 92];
+let nums2 = [-89, 92, -50, 13, -44, 16, 1, -63, -85, -47, 16, -3, -63, -43, 32, 40, -5, 2, 14, -39];
 
-console.log(intersectionOfTwoArrays(4,nums1,3,nums2));
+console.log(intersectionOfTwoArrays(20,nums1,20,nums2));
